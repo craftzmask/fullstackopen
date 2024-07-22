049228f8378ca072e91b0d6d4ac88b910dfad0d1
Khanh Chung <khanh.d.chung@gmail.com>
2024-07-21 19:13:22 -0700

    1.9: unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,10 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good - bad) / all
   const positive = good / all * 100
 
+  if (all === 0) {
+    return <div>No feedback given</div>
+  }
+
   return (
     <>
       <div>good {good}</div>
