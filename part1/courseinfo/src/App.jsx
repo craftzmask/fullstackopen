49ff756ab526e03762eb4312d7caee0a28d7b3b0
Khanh Chung <khanh.d.chung@gmail.com>
2024-07-21 20:57:21 -0700

    2.3*: Course information step 8

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -19,10 +19,8 @@ const Content = ({ parts }) => {
 }
 
 const Total = ({ parts }) => {
-  let total = 0
-  for (let i = 0; i < parts.length; i++) {
-    total += parts[i].exercises
-  }
+  let total = parts
+    .reduce((sum, part) => sum + part.exercises, 0)
 
   return (
     <p>
