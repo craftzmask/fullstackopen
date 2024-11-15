699737c6080de532873e51231b9d1af43cf33292
Khanh Chung <khanh.d.chung@gmail.com>
2024-11-14 22:55:17 -0800

    2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [name, setName] = useState('')
   const [number, setNumber] = useState('')
+  const [search, setSearch] = useState('')
+
+  const personsToShow = persons.filter(p => {
+    const name = p.name.toLowerCase()
+    return name.includes(search.toLowerCase())
+  })
 
   const handleSubmit = e => {
     e.preventDefault()
@@ -20,6 +29,12 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input
+          value={search}
+          onChange={e => setSearch(e.target.value)} />
+      </div>
       <form onSubmit={handleSubmit}>
         <div>
           name:
@@ -38,7 +53,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(p =>
+      {personsToShow.map(p =>
         <div key={p.name}>{p.name} {p.number}</div>
       )}
     </div>
