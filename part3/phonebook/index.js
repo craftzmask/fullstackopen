88118833efc1817a0ce7140b0f5a1924705b8949
Khanh Chung <khanh.d.chung@gmail.com>
2024-11-15 23:23:47 -0800

    3.15: Phonebook database, step 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -35,9 +35,9 @@ app.post('/api/persons', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(p => p.id !== id)
-  res.status(204).end()
+  Person
+    .findByIdAndDelete(req.params.id)
+    .then(() => res.status(204).end())
 })
 
 app.get('/info', (req, res) => {
