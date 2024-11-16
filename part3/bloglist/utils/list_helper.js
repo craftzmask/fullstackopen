ddf20eb69b85776e8ed4ad38473702d2f858b18f
Khanh Chung <khanh.d.chung@gmail.com>
2024-11-16 14:49:39 -0800

    4.5*: Helper Functions and Unit Tests, step 3

--- part3/bloglist/utils/list_helper.js
+++ part3/bloglist/utils/list_helper.js
@@ -4,6 +4,23 @@ const totalLikes = blogs => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) return null
+
+  let mostLikeIndex = 0
+  for (let i = 1; i < blogs.length; i++) {
+    if (blogs[mostLikeIndex].likes < blogs[i].likes) {
+      mostLikeIndex = i
+    }
+  }
+
+  return {
+    title: blogs[mostLikeIndex].title,
+    author: blogs[mostLikeIndex].author,
+    likes: blogs[mostLikeIndex].likes
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
