f2672d876e96695866160a5c5c80208f2ddd6b35
Khanh Chung <khanh.d.chung@gmail.com>
2024-11-17 17:45:00 -0800

    5.7 Blog List Frontend, step 7

--- part5/bloglist/src/components/Blog.jsx
+++ part5/bloglist/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
 
+const Blog = ({ blog }) => {
+  const [showDetail, setShowDetail] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const detail = () => (
+    <div>
+      <div>{blog.url}</div>
+      <div>
+        likes: {blog.likes}
+        <button>like</button>
+      </div>
+      <div>{blog.user && blog.user.name}</div>
+    </div>
+  )
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setShowDetail(!showDetail)}>
+          {showDetail ? 'hide' : 'view'}
+        </button>
+      </div>
+      {showDetail && detail()}
+    </div>
+  )
+}
 export default Blog
