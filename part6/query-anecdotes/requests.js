52469e7605ddf25846e7df049ec3a32b890bb9e9
Khanh Chung <khanh.d.chung@gmail.com>
2024-11-20 22:42:54 -0800

    Exercise 6.21

--- part6/query-anecdotes/requests.js
+++ part6/query-anecdotes/requests.js
@@ -5,3 +5,13 @@ const baseUrl = 'http://localhost:3001/anecdotes'
 export const getAnecdotes = () => {
   return axios.get(baseUrl).then(res => res.data)
 }
+
+export const createAnecdote = anecdote => {
+  return axios.post(baseUrl, anecdote).then(res => res.data)
+}
+
+export const udpateAnecdote = anecdote => {
+  return axios
+    .put(`${baseUrl}/${anecdote.id}`, anecdote)
+    .then(res => res.data)
+}
