0c3b86401ea12c252a47330a6bd6abdde0aa3ed1
Khanh Chung <khanh.d.chung@gmail.com>
2024-11-21 22:20:29 -0800

    7.6: Anecdotes and Hooks step 3

--- part7/routed-anecdotes/src/components/AnecdoteForm.jsx
+++ part7/routed-anecdotes/src/components/AnecdoteForm.jsx
@@ -2,9 +2,9 @@ import { useNavigate } from 'react-router-dom'
 import { useField } from '../hooks'
 
 const AnecdoteForm = (props) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const { reset: contentReset, ...content } = useField('text')
+  const { reset: authorReset, ...author } = useField('text')
+  const { reset: infoReset, ...info } = useField('text')
 
   const navigate = useNavigate()
 
@@ -20,9 +20,9 @@ const AnecdoteForm = (props) => {
   }
 
   const reset = () => {
-    content.reset()
-    author.reset()
-    info.reset()
+    contentReset()
+    authorReset()
+    infoReset()
   }
 
   return (
