b985253ad290fbe2d66a2686c9ecf20ed23d535d
Khanh Chung <khanh.d.chung@gmail.com>
2025-07-30 01:00:22 -0700

    2.12: The Phonebook step 7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -28,11 +28,14 @@ const App = () => {
       return
     }
 
-    setPersons(persons.concat({
-      name: newName, number: newNumber
-    }))
-    setNewName('')
-    setNewNumber('')
+    const personObject = { name: newName, number: newNumber }
+    axios
+      .post("http://localhost:3001/persons", personObject)
+      .then(res => {
+        setPersons(persons.concat(res.data))
+        setNewName('')
+        setNewNumber('')
+      })
   }
 
   return (
