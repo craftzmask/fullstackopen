27c771c3f1b1f256f1c8d801b372559e5f8c70b6
Khanh Chung <khanh.d.chung@gmail.com>
2025-07-30 22:39:49 -0700

    3.3: Phonebook backend step 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,16 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
 app.get('/info', (req, res) => {
   res.send(
     `
@@ -36,6 +46,7 @@ app.get('/info', (req, res) => {
     `
   )
 })
+
 
 const PORT = 3001
 app.listen(PORT, () => {
