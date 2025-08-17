26eb85b9345b8e5fa47262bd668695610a67a53a
Khanh Chung <khanh.d.chung@gmail.com>
2025-08-16 17:42:33 -0700

    Exercise 6.20

---
+++ part6/query-anecdotes/src/App.jsx
@@ -0,0 +1,45 @@
+import { useQuery } from '@tanstack/react-query'
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+
+
+const App = () => {
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: anecdoteService.getAnecdotes
+  })
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  if (result.isLoading) {
+    return <p>Loading...</p>
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
