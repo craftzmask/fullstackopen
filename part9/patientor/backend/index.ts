3df12b76c62e16b0c5085c36a47c97d259837c2b
Khanh Chung <khanh.d.chung@gmail.com>
2025-08-21 19:04:10 -0700

    9.10: Patientor backend, step3

--- part9/patientor/backend/index.ts
+++ part9/patientor/backend/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 const app = express();
 
+import diagnosisService from "./services/diagnosisService";
+
 app.use(express.json());
 
 const PORT = 3001;
 
 app.get("/api/ping", (_req, res) => {
   res.send("ping");
+});
+
+app.get("/api/diagnoses", (_req, res) => {
+  res.json(diagnosisService.getEntries());
 });
 
 app.listen(PORT, () => {
---
+++ part9/patientor/backend/services/diagnosisService.ts
@@ -0,0 +1,11 @@
+import diagnosisData from "../data/diagnoses";
+
+import { Diagnosis } from "../types";
+
+const getEntries = (): Diagnosis[] => {
+  return diagnosisData;
+};
+
+export default {
+  getEntries,
+};
