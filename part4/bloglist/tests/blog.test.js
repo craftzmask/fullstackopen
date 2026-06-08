79e6bad15cc59d6a66730dcd90195801bae57f02
Khanh Chung <khanh.d.chung@gmail.com>
2026-06-08 00:39:06 -0700

    4.4: Helper Functions and Unit Tests, step 2

--- part4/bloglist/tests/blog.test.js
+++ part4/bloglist/tests/blog.test.js
@@ -6,3 +6,22 @@ test("dummy return one", () => {
   const result = listHelper.dummy([]);
   assert.strictEqual(result, 1);
 });
+
+describe("total likes", () => {
+  test("0 blogs equals 0", () => {
+    const result = listHelper.totalLikes([]);
+    assert.strictEqual(result, 0);
+  });
+
+  test("1 blog equals to that blog's likes", () => {
+    const blogs = listHelper.blogs.slice(0, 1);
+    const result = listHelper.totalLikes(blogs);
+    assert.strictEqual(result, blogs[0].likes);
+  });
+
+  test("blogs equals their likes sum", () => {
+    const sum = listHelper.blogs.reduce((sum, blog) => sum + blog.likes, 0);
+    const result = listHelper.totalLikes(listHelper.blogs);
+    assert.strictEqual(result, sum);
+  });
+});
