571a6571874a39f6a32adc2733e9550f3eeab208
Khanh Chung <khanh.d.chung@gmail.com>
2026-07-03 19:56:05 -0700

    5.14: Blog List Tests, step 2

--- part5/bloglist/src/components/Blog.test.jsx
+++ part5/bloglist/src/components/Blog.test.jsx
@@ -1,22 +1,35 @@
+import { beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
+const user = {
+  username: "username",
+  name: "name",
+};
+
+const blog = {
+  title: "React patterns",
+  author: "Michael Chan",
+  url: "https://reactpatterns.com/",
+  likes: 7,
+};
+
+beforeEach(() => {
+  const mockHandler = vi.fn();
+  render(<Blog user={user} blog={blog} onLikeClick={mockHandler} />);
+});
+
 test("render blog", () => {
-  const user = {
-    username: "username",
-    name: "name",
-  };
-
-  const blog = {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-  };
-
-  render(<Blog user={user} blog={blog} />);
-
   expect(screen.getByText(`${blog.title} ${blog.author}`)).toBeVisible();
   expect(screen.getByText(blog.url, { exact: false })).not.toBeVisible();
   expect(screen.getByText(blog.likes, { exact: false })).not.toBeVisible();
+});
+
+test("show url and likes when user click show button", async () => {
+  const user = userEvent.setup();
+  const button = screen.getByText("show");
+  await user.click(button);
+  expect(screen.getByText(blog.url, { exact: false })).toBeVisible();
+  expect(screen.getByText(blog.likes, { exact: false })).toBeVisible();
 });
