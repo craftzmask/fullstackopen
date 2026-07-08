d65baf004f9caa25aa030e333a30f47202d0117e
Khanh Chung <khanh.d.chung@gmail.com>
2026-07-08 10:18:07 -0700

    6.5: anecdotes, step4

--- part6/zustand-anecdotes/src/components/AnecdoteList.jsx
+++ part6/zustand-anecdotes/src/components/AnecdoteList.jsx
@@ -3,10 +3,11 @@ import { useAnecdotes, useAnecdoteActions } from "../store";
 const AnecdoteList = () => {
   const anecdotes = useAnecdotes();
   const { vote } = useAnecdoteActions();
+  const sortedAnedotes = anecdotes.toSorted((a, b) => b.votes - a.votes);
 
   return (
     <div>
-      {anecdotes.map((anecdote) => (
+      {sortedAnedotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
