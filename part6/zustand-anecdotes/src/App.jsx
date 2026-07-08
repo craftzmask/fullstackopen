709d0a88c5a2f17300943d52aa79f1e9e77b7835
Khanh Chung <khanh.d.chung@gmail.com>
2026-07-08 10:16:19 -0700

    6.4: anecdotes, step3

--- part6/zustand-anecdotes/src/App.jsx
+++ part6/zustand-anecdotes/src/App.jsx
@@ -1,34 +1,12 @@
-import { useAnecdoteActions, useAnecdotes } from "./store";
+import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
 
 const App = () => {
-  const anecdotes = useAnecdotes();
-  const { add, vote } = useAnecdoteActions();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    add(e.target.anecdote.value);
-    e.target.reset();
-  };
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
-      <h2>create new</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input name="anecdote" />
-        </div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   );
 };
