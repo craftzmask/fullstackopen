aa1b40dac5040527e977499c55b17a6b3cb62cce
Khanh Chung <khanh.d.chung@gmail.com>
2026-07-10 10:36:00 -0700

    6.7 anecdotes, step6

--- part6/zustand-anecdotes/src/App.jsx
+++ part6/zustand-anecdotes/src/App.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
+import anecdoteService from "./services/anecdotes";
+import { useAnecdoteActions } from "./store";
 
 const App = () => {
+  const { initialize } = useAnecdoteActions();
+
+  useEffect(() => {
+    anecdoteService.getAll().then((data) => initialize(data));
+  }, []);
+
   return (
     <div>
       <Filter />
